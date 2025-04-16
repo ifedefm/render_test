ead48316c54d12fd9f057b1f303422f08abddca8
Braulio Pierotti <148255441+PierottiBR@users.noreply.github.com>
2025-04-16 07:39:41 -0300

    fix csv

--- funciones_gencb.py
+++ funciones_gencb.py
@@ -152,7 +152,8 @@ def actualizar_csv_pago(usuario: str, monto: float, commit_message="Actualizar r
     if usuario in df["usuario"].values:
         df.loc[df["usuario"] == usuario, "monto_cargado_hasta_la_fecha"] += monto
     else:
-        df = pd.concat({"usuario": usuario, "monto_cargado_hasta_la_fecha": monto}, ignore_index=True)
+        nueva_fila = pd.DataFrame([{"usuario": usuario, "monto_cargado_hasta_la_fecha": monto}])
+        df = pd.concat([df, nueva_fila], ignore_index=True)
 
     # Convertir de nuevo a CSV
     csv_updated = df.to_csv(index=False)
